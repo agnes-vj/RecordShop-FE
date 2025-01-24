ac9783b03b3eb0599641bfb1278ea892f4d9d5d2
Agnes <133767448+agnes-vj@users.noreply.github.com>
2025-01-24 13:37:37 +0000

    Add RecordShopApiClent

---
+++ RecordShop-Frontend/Services/RecordShopApiClient.cs
@@ -0,0 +1,35 @@
+﻿using RecordShop_Frontend.Model;
+
+namespace RecordShop_Frontend.Services
+{
+    public class RecordShopApiClient
+    {
+         string BASE_URL;
+    public RecordShopApiClient(IConfiguration configuration)
+        {
+            BASE_URL = configuration["BACKEND_URL:Dev"];
+            Console.WriteLine("BASE" +BASE_URL);
+        }
+        public async Task<List<AlbumDTO>> GetAlbums()
+        {
+            
+            using (HttpClient httpClient = new HttpClient())
+            {
+                string url = $"{BASE_URL}/api/albums";
+                Console.WriteLine("URL :" + url);
+                try
+                {
+                    List<AlbumDTO> albums = await httpClient.GetFromJsonAsync<List<AlbumDTO>>(url);
+                    if (albums != null)
+                        return albums;
+
+                }
+                catch (HttpRequestException error)
+                {
+                    Console.WriteLine($"Error: {error.Message}");                   
+                }
+            }
+            return null;
+        }
+    }
+}
