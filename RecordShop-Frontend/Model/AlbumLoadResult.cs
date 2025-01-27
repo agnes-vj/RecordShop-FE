db4cfed1e0fdfb1b3b22277c40fb8330c5611309
Agnes <133767448+agnes-vj@users.noreply.github.com>
2025-01-27 16:37:29 +0000

    Add wrapper class AlbumLoadResults to hold the data return by AlbumsApiClient

---
+++ RecordShop-Frontend/Model/AlbumLoadResult.cs
@@ -0,0 +1,9 @@
+﻿using RecordShop_Frontend.Model;
+
+public class AlbumLoadResult
+{
+    public List<AlbumDTO> Albums { get; set; }
+    public bool HasError { get; set; }
+    public string ErrorMessage { get; set; }
+
+}
